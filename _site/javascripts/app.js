94004e5d7a6f7760d7955f342e8aed792f94e06d
Wesley Crook <wesley.d.crook@ehi.com>
2019-01-10 14:45:08 -0500

    Slowed services carousel from 3 to 4 seconds

--- _site/javascripts/app.js
+++ _site/javascripts/app.js
@@ -280,7 +280,7 @@ jQuery(document).foundation();
         slidesToShow: 3,
         slidesToScroll: 1,
         autoplay: true,
-        autoplaySpeed: 3000,
+        autoplaySpeed: 4000,
         pauseOnHover: false,
         responsive: [
           {
